de402dc24756a81c7222b30acab2f4c0229801b2
JoeMc553 <josephmccurtain553@gmail.com>
2024-11-04 23:33:54 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,6 @@ const Navbar = () => {
     <div>
         <Link to = '/' >Home</Link>
         <Link to = '/dashboard' >Dashboard</Link>
-        <Link to = '/Contact' >contact</Link>
     </div>
   )
 }
