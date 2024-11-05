7967b315e28c24cb94ad40a99c8db4356c16ba45
JoeMc553 <josephmccurtain553@gmail.com>
2024-11-04 23:33:31 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import {RouterProvider, createBrowserRouter} from "react-router-dom";
 import Dashboard from "./routes/Dashboard.jsx"
-import Contact from "./routes/contact.jsx"
 import Signup from "./routes/Signup.jsx"
 
 const router = createBrowserRouter([
   { path: '/', element: <App />},
   { path: '/dashboard', element: <Dashboard />},
-  { path: '/contact', element: <Contact />},
   { path: '/signup', element: <Signup />},
 ]);
 
